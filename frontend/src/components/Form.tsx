b4f9e32151223022c0d943df05d691c99cde4a3f
junabel <jnlmyz@gmail.com>
2022-01-22 15:14:19 +0900

    update form component using state

--- frontend/src/components/Form.tsx
+++ frontend/src/components/Form.tsx
@@ -1,7 +1,17 @@
+import { useState } from 'react';
+
 const Form = () => {
+    //ユーザが入力した都市名を保管する場所をcity
+    // データを操作する仕組みがsetCity
+    const [city, setCity] = useState<string>("");
     return (
         <form>
-            <input type="text" name="city" placeholder="都市名"/>
+            {/* eはイベントパラメーターでinputに関する様々な情報が格納されている */}
+            {/* eをsetCityへと=>を使って私、setCity内でValueへアクセスする */}
+            <input type="text" name="city" placeholder="都市名" onChange={e => setCity(e.target.value)}/>
+            {city}
+            {/* {}でstateを呼び込む */}
+            {/* 追加 */}
             <button type="submit">Get Weather</button>
         </form>
     );
