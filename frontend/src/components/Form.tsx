c6e5e64c9ae2ecf369e5d51f3e13e9eabfdfe383
junabel <jnlmyz@gmail.com>
2022-01-22 16:26:11 +0900

    update:use weather api

--- frontend/src/components/Form.tsx
+++ frontend/src/components/Form.tsx
@@ -1,20 +1,23 @@
 import { useState } from 'react';
 
-"http://api.weatherapi.com/v1/current.json?key=zzzz&q=London&aqi=no"
-
 const Form = () => {
     //ユーザが入力した都市名を保管する場所をcity
     // データを操作する仕組みがsetCity
     const [city, setCity] = useState<string>("");
+    // 関数宣言、アロー関数を使用している
+    const getWeather = (e: any) => {
+        e.preventDefault();
+        fetch("http://api.weatherapi.com/v1/current.json?key=&q=London&aqi=no")
+            .then(res => res.json())
+            //dataが分からない
+            .then(data => console.log(data))
+    }
     return (
         <form>
             {/* eはイベントパラメーターでinputに関する様々な情報が格納されている */}
             {/* eをsetCityへと=>を使って私、setCity内でValueへアクセスする */}
             <input type="text" name="city" placeholder="都市名" onChange={e => setCity(e.target.value)}/>
-            {city}
-            {/* {}でstateを呼び込む */}
-            {/* 追加 */}
-            <button type="submit">Get Weather</button>
+            <button type="submit" onClick={getWeather}>Get Weather</button>
         </form>
     );
 };
