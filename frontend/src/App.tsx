78da7d77490075523515482b6d625959b8c7c4df
junabel <jnlmyz@gmail.com>
2022-01-22 14:09:07 +0900

    update app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,7 +15,6 @@ function App() {
         >
           Learn React
         </a>
-      </header>
     </div>
   );
 }
