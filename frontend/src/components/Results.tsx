90761e0568938041a68cbc0e24292e82ad5a7079
junabel <jnlmyz@gmail.com>
2022-01-22 21:55:48 +0900

    ref:remove div from Results component

--- frontend/src/components/Results.tsx
+++ frontend/src/components/Results.tsx
@@ -12,7 +12,7 @@ const Results = ({ results }: ResultsPropsType) => {
     const { cityName, country, temperature, conditionText, icon } = results;
     return (
         <>
-            {cityName && <div>{cityName}</>}
+            {cityName && <div>{cityName}</div>}
             {country && <div>{country}</div>}
             {temperature && <div>{temperature}</div>}
             {conditionText &&
