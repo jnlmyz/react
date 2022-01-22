16c596b7c3d8778692f743089e2740db044653f5
junabel <jnlmyz@gmail.com>
2022-01-22 14:21:03 +0900

    add Title component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,19 +2,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="test">
+      <h1>こんにちは！</h1>
     </div>
   );
 }
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
+  // エラーなどを検出してくれる
   <React.StrictMode>
     <App />
   </React.StrictMode>,
