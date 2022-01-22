6e30965aad9c723e5cf2759e6fd9c8b0b5cf38e1
junabel <jnlmyz@gmail.com>
2022-01-22 16:51:21 +0900

    update: implement source of truth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,26 @@
+import { useState } from 'react';
 import Title from "./components/Title";
 import Form from "./components/Form";
+import Results from "./components/Results"
 import './App.css';
 
 function App() {
-  return (
-    <div className="test">
-      <Title />
-      <Form />
-    </div>
-  );
+    const [city, setCity] = useState<string>("");
+    const getWeather = (e: any) => {
+        e.preventDefault();
+        fetch("http://api.weatherapi.com/v1/current.json?key=&q=London&aqi=no")
+            .then(res => res.json())
+            //dataが分からない
+            .then(data => console.log(data))
+    }
+
+    return (
+      <div className="test">
+        <Title cityName={city}/>
+        <Form setCity={setCity} getWeather={getWeather}/>
+        <Results />
+      </div>
+    );
 }
 
 export default App;
--- frontend/src/components/Form.tsx
+++ frontend/src/components/Form.tsx
@@ -1,23 +1,8 @@
-import { useState } from 'react';
-
-const Form = () => {
-    //ユーザが入力した都市名を保管する場所をcity
-    // データを操作する仕組みがsetCity
-    const [city, setCity] = useState<string>("");
-    // 関数宣言、アロー関数を使用している
-    const getWeather = (e: any) => {
-        e.preventDefault();
-        fetch("http://api.weatherapi.com/v1/current.json?key=&q=London&aqi=no")
-            .then(res => res.json())
-            //dataが分からない
-            .then(data => console.log(data))
-    }
+const Form = (props: any) => {
     return (
         <form>
-            {/* eはイベントパラメーターでinputに関する様々な情報が格納されている */}
-            {/* eをsetCityへと=>を使って私、setCity内でValueへアクセスする */}
-            <input type="text" name="city" placeholder="都市名" onChange={e => setCity(e.target.value)}/>
-            <button type="submit" onClick={getWeather}>Get Weather</button>
+            <input type="text" name="city" placeholder="都市名" onChange={e => props.setCity(e.target.value)}/>
+            <button type="submit" onClick={props.getWeather}>Get Weather</button>
         </form>
     );
 };
